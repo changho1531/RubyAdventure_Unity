03f45eb978686e2a1946b3dfd2a7aa2a6e98a9da
changho1531 <98195821+changho1531@users.noreply.github.com>
2023-01-06 21:40:57 +0900

    테스트#1

--- Assets/Scripts/gitTest.cs
+++ Assets/Scripts/gitTest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test1 : MonoBehaviour
+public class gitTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
